c9aa2950683d6d2863cfc90d97b4d200a787aec9
Elijah <bundielly@gmail.com>
2022-06-17 14:29:27 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    let num1 = array[i]
+    // console.log(num1)
+    for (let j = i + 1; j < array.length; j++) {
+      let num2 = array[j]
+      // console.log(num2)
+      let sum = num1 + num2
+      if (sum === target) {
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /* 
@@ -8,10 +21,15 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  use a for loop to iterate through all the numbers 
+  use a another for loop to iterate through the adjacent numbers
+  add the two items and test
 */
 
 /*
   Add written explanation of your solution here
+  Iterate through all the numbers using a loop and 
+  adding them up with each other until the number is found.
 */
 
 // You can run `node index.js` to view these console logs
@@ -32,3 +50,5 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+
+
